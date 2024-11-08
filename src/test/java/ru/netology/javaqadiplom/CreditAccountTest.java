71fb97400f68d211ec337b9c171be6a070a4b660
Dmitriz1 <stud0000264980@study.utmn.ru>
2024-11-08 14:00:12 +0500

    tests for credit made

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class CreditAccountTest {
 
     @Test
-    public void shouldAddToPositiveBalance() {
+    public void shouldAddToZeroBalance() {
         CreditAccount account = new CreditAccount(
                 0,
                 5_000,
@@ -16,5 +16,52 @@ public class CreditAccountTest {
         account.add(3_000);
 
         Assertions.assertEquals(3_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(4_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldNotAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -1_000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(false, account.getBalance());
+    }
+
+    @Test
+    public void shouldCountYearChangeCorrectly() {
+        CreditAccount account = new CreditAccount(
+                200,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void payExceedingLimit() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                500,
+                10);
+
+        Assertions.assertEquals(false, account.pay(1_200));
     }
 }
